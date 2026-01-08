bd87eed2e87c350064facdbf40603c99ff7c321c
noname7321 <1755247252@qq.com>
2026-01-08 16:13:01 +0800

    6

--- string.cpp
+++ string.cpp
@@ -1,4 +1,27 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include <cstring>
 
+int main()
+{
+    const char* cstr = "Hello, World!";//C-style string
+    strlen(cstr); //calculate the length of C-style string
+    //const means the content(here is "Hello, World!") pointed to by cstr cannot be modified,but cstr can point to another string
+    std::string str1 = "Hello, C++23!";//C++ string
+    str1.size(); //calculate the length of C++ string
+    str1+= " Let's learn about strings.";//concatenate C++ strings
+    char name1[] = "Hello"; //C-style string using char array
+    char name2[6]=  {'W','o','r','l','d','\0'}; //C-style string using char array
+    //'\0' is the null terminator indicating the end of the string
+    //''is used for single characters,"" is used for string literals
+    std::string str2 =std::string(name1) + " " + std::string(name2)+ std::string("!"); 
+    //convert C-style strings to C++ strings and concatenate
+
+    std::cout << name1 << std::endl;
+    std::cout << name2 << std::endl;
+    std::cout << cstr << std::endl;
+    std::cout << str1 << std::endl;
+    std::cout << str2 << std::endl;
+    
+}
