71c7f331c8bacc849214758618eecb85a69fdec5
noname7321 <112142333+noname7321@users.noreply.github.com>
2026-01-11 18:14:31 +0800

    9

--- mutable.cpp
+++ mutable.cpp
@@ -13,3 +13,31 @@ public:
         return accessCount;
     }
 };
+
+class entity {
+private:
+    std::string name;
+public:
+    const std::string& getName() const {//reference here means we are not making a copy,but returning the original
+        return name;
+    }
+    void setName(const std::string& newName) {
+        name = newName;
+    }
+};
+
+int main()
+{
+    const entity e;
+    e.getName(); // Calling const method
+    //非 const 成员函数不能在 const 对象上被调用
+
+    int x=9;
+    auto f= [&]() mutable
+    {
+        std::cout<<x<<std::endl;
+        x=10; // lambda 捕获的变量默认是不可变的，除非使用 mutable 关键字 
+    };
+    f();
+    std::cout<<x<<std::endl; // x remains 9 outside the lambda
+}
