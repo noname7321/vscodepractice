e226220de5fbfc100a226dacad008afd542e4e55
noname7321 <1755247252@qq.com>
2026-01-27 19:48:41 +0800

    13

--- python/string.py
+++ python/string.py
@@ -1,3 +1,5 @@
 first_name=input("Enter your first name: ")
 last_name=input("Enter your last name: ")
 print("Hello, " + first_name.capitalize() + " " + last_name.capitalize() + "! Welcome to the program.")
+print('Hello, {} {}! Welcome to the program.'.format(first_name.capitalize(), last_name.capitalize()))
+print(f'Hello, {first_name.capitalize()} {last_name.capitalize()}! Welcome to the program.')
