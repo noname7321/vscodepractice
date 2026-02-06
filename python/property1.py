d4920d1a25c238381481d94c0310b6b6b697d860
noname7321 <112142333+noname7321@users.noreply.github.com>
2026-02-06 10:24:58 +0800

    30

--- python/property1.py
+++ python/property1.py
@@ -5,6 +5,16 @@ class Student:
     @property # transforming method into property, so we can access it like an attribute
     def age(self):
         return self.__age
+    @age.setter
+    def age(self, age):
+        if age >= 0:
+            self.__age = age
+        else:
+            print("Age cannot be negative")
+
 stu=Student("John", 25)
 print(stu.name)  # Accessing public attribute
 print(stu.age)   # Accessing private attribute via property
+stu.age = 30     # Modifying private attribute via property setter
+print(stu.age)   # Accessing modified private attribute via property
+
